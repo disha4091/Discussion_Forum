501f01ea829561116ec9e6659d963463bfb24a6e
Disha <disha4091@gmail.com>
2021-06-20 00:17:58 +0530

    Responsive navbar

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import "./nav.css";
 import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
+  const [showlinks, setShowLinks] = useState(false) ;
   return (
+
     <div className="nav">
-    <div class="ui large secondary inverted pointing menu ">
-      <NavLink to="/" className="nav-link" activeClassName="currentpage">
+    
+    <button onClick={()=>setShowLinks(!showlinks)} className="menuIcon"><i class="align justify icon"></i></button>
+  
+      <NavLink to="/" className="nav-link" activeClassName="currentpage" id={showlinks ? "hidden" : ""}  onClick={()=>setShowLinks(!showlinks)}>
          Home
       </NavLink>
-      
-      <div className="ui simple dropdown item">
-        <p className="domain">Domains</p>
-        <i class="dropdown icon"></i>
-        <div class="menu">
-            <Link to="/mlposts" class="item">Machine Learning</Link>
-            <Link to="/webposts" class="item">Web Development</Link>          
-            <Link to="/aiposts" class="item">Artificial Intelligence</Link>
+  
+     <div class="ui dropdown" id={showlinks ? "hidden" : ""} >
+     <div class="ui dropdown" id={showlinks ? "hidden" : ""}>
+        <div class="ui simple dropdown item"  id={showlinks ? "hidden" : ""}>Domains
+          <i class="dropdown icon"></i>
+          <div class="menu">
+                  <Link to="/mlposts" class="item"  onClick={()=>setShowLinks(!showlinks)}>Machine Learning</Link>
+                  <Link to="/webposts" class="item"  onClick={()=>setShowLinks(!showlinks)}>Web Development</Link>          
+                  <Link to="/aiposts" class="item"  onClick={()=>setShowLinks(!showlinks)}>Artificial Intelligence</Link>
+              </div>
         </div>
       </div>
+     </div>
+      
+      
 
-      <div class="right menu">
-        <NavLink
+
+      <div className="rightSide">
+      <NavLink
         to="/login"
         className="nav-link"
         activeClassName="currentpage"
+        id={showlinks?"hidden" : ""}
+        onClick={()=>setShowLinks(!showlinks)}
         >
             Login
           </NavLink>
@@ -31,12 +43,15 @@ const Navbar = () => {
             to="/signup"
             className="nav-link"
             activeClassName="currentpage"
+            id={showlinks?"hidden" : ""}
+            onClick={()=>setShowLinks(!showlinks)}
           >
             Signup
           </NavLink>
         </div>
       </div>
-    </div>
+   
+        
   );
 };
 
